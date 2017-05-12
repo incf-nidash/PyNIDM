ce1ab55be17f356e7cd50ea1019fd433575f3f04
David Keator <dbkeator@uci.edu>
2017-05-12 11:04:41 -0700

    updated structure per Satra suggestions

---
+++ nidm/experiment/tests/test.py
@@ -0,0 +1,16 @@
+import os,sys
+
+from NIDMExperiment import NIDMExperimentProject
+
+nidm_doc = NIDMExperimentProject()
+inv = nidm_doc.addProject("FBIRN_PhaseII","9610","Test investigation")
+#test add string attribute
+nidm_doc.addLiteralAttribute(inv,"nidm","isAwesome","15")
+#test add float attribute
+nidm_doc.addLiteralAttribute(inv,"nidm", "score", float(2.34))
+#test add long attribute
+nidm_doc.addLiteralAttribute(inv, "nidm", "value", long(13412341235))
+#test add PI to investigation
+nidm_doc.addProjectPI(inv,"Keator", "David")
+print (nidm_doc.serializeTurtle())
+#print nidm_doc.serializeJSONLD()
