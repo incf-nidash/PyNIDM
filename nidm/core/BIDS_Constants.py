1b71402f6b44ce3347270cf3840c4af2d76336f6
David Keator <dbkeator@uci.edu>
2017-06-04 19:28:18 -0700

    Added contants file for BIDS terms mappingsd, modified examples to use constants instead of hard coded unames

---
+++ nidm/core/BIDS_Constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+''' BIDS Terms -> NIDM-Exp Mappings
+@author: David Keator <dbkeator@uci.edu>
+'''
+from nidm.core import Constants
+
+BIDSVersion = Constants.NIDM_PROJECT_IDENTIFIER
+Name = Constants.NIDM_PROJECT_NAME
+Procedure = Constants.NIDM_PROJECT_DESCRIPTION
+License = Constants.NIDM_PROJECT_LICENSE
+ReferencesAndLinks = Constants.NIDM_PROJECT_REFERENCES
+Authors = ReferencesAndLinks
