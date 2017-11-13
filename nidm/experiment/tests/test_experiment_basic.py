00ba742c2d31da429f6ffe910b2172c35b67a0de
Dorota Jarecka <djarecka@gmail.com>
2017-11-13 15:48:30 -0500

    testing

--- nidm/experiment/tests/test_experiment_basic.py
+++ nidm/experiment/tests/test_experiment_basic.py
@@ -4,6 +4,9 @@ import pytest, pdb
 from nidm.experiment import Project,Session,Acquisition,AcquisitionObject
 from nidm.core import Constants
 
+def test_1():
+    pass
+
 def test_1(tmpdir):
     tmpdir.chdir()
 
