6f7b405bc6fb89bf509b2f486c05008c0ddaaede
David Keator <dbkeator@uci.edu>
2018-03-22 11:22:25 -0700

    added support for variables with spaces and json-ld export to BIDSMRI2NIDM.py

--- nidm/experiment/tools/BIDSMRI2NIDM.py
+++ nidm/experiment/tools/BIDSMRI2NIDM.py
@@ -45,6 +45,7 @@ import csv
 import glob
 from argparse import ArgumentParser
 from bids.grabbids import BIDSLayout
+from urllib.parse import quote
 
 def getRelPathToBIDS(filepath, bids_root):
     """
@@ -67,11 +68,16 @@ def main(argv):
         and link to the associated json data dictionary file.')
 
     parser.add_argument('-d', dest='directory', required=True, help="Path to BIDS dataset directory")
+    parser.add_argument('-jsonld', '--jsonld', action='store_true', help='If flag set, output is json-ld not TURTLE')
     parser.add_argument('-o', dest='outputfile', default="nidm.ttl", help="NIDM output turtle file")
     args = parser.parse_args()
 
     directory = args.directory
     outputfile = args.outputfile
+
+    #if we're choosing json-ld, make sure file extension is .json
+    if args.jsonld:
+        outputfile = os.path.splitext(outputfile)[0]+".json"
 
     #importlib.reload(sys)
     #sys.setdefaultencoding('utf8')
@@ -133,7 +139,8 @@ def main(argv):
                         acq_entity.add_attributes({BIDS_Constants.participants[key]:value})
                 #else just put variables in bids namespace since we don't know what they mean
                 else:
-                    acq_entity.add_attributes({Constants.BIDS[key]:value})
+                    #acq_entity.add_attributes({Constants.BIDS[quote(key)]:value})
+                    acq_entity.add_attributes({Constants.BIDS[key.replace(" ", "_")]:value})
 
 
 
@@ -189,9 +196,9 @@ def main(argv):
                     for key in json_data:
                         if key in BIDS_Constants.json_keys:
                             if type(json_data[key]) is list:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:''.join(str(e) for e in json_data[key])})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:''.join(str(e) for e in json_data[key])})
                             else:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:json_data[key]})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:json_data[key]})
             elif file_tpl.modality == 'func':
                 #do something with functionals
                 acq_obj = MRObject(acq)
@@ -218,9 +225,9 @@ def main(argv):
                     for key in json_data:
                         if key in BIDS_Constants.json_keys:
                             if type(json_data[key]) is list:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:''.join(str(e) for e in json_data[key])})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:''.join(str(e) for e in json_data[key])})
                             else:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:json_data[key]})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:json_data[key]})
 
                 #get associated events TSV file
                 if 'run' in file_tpl._fields:
@@ -263,9 +270,9 @@ def main(argv):
                     for key in json_data:
                         if key in BIDS_Constants.json_keys:
                             if type(json_data[key]) is list:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:''.join(str(e) for e in json_data[key])})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:''.join(str(e) for e in json_data[key])})
                             else:
-                                acq_obj.add_attributes({BIDS_Constants.json_keys[key]:json_data[key]})
+                                acq_obj.add_attributes({BIDS_Constants.json_keys[key.replace(" ", "_")]:json_data[key]})
 
                 #for bval and bvec files, what to do with those?
 
@@ -319,7 +326,10 @@ def main(argv):
     #serialize graph
     #print(project.graph.get_provn())
     with open(outputfile,'w') as f:
-        f.write(project.serializeTurtle())
+        if args.jsonld:
+            f.write(project.serializeJSONLD())
+        else:
+            f.write(project.serializeTurtle())
         #f.write(project.graph.get_provn())
     #save a DOT graph as PNG
     project.save_DotGraph(str(outputfile + ".png"),format="png")
