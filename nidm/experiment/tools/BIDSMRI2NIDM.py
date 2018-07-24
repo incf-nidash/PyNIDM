6787dd7728e2db51db243253d82f7ab33da35e41
David Keator <dbkeator@uci.edu>
2018-07-24 16:25:19 -0700

    modified help in BIDSMRI2NIDM.py

--- nidm/experiment/tools/BIDSMRI2NIDM.py
+++ nidm/experiment/tools/BIDSMRI2NIDM.py
@@ -44,6 +44,7 @@ from prov.model import QualifiedName
 from prov.model import Namespace as provNamespace
 from nidm.experiment.Core import Core
 from os.path import isfile
+from argparse import RawTextHelpFormatter
 import json
 import logging
 import csv
@@ -67,24 +68,46 @@ def getRelPathToBIDS(filepath, bids_root):
 
 
 def main(argv):
-    parser = ArgumentParser(description='This program will convert a BIDS MRI dataset to a NIDM-Experiment \
-        RDF document.  It will parse phenotype information and simply store variables/values \
-        and link to the associated json data dictionary file.')
+    parser = ArgumentParser(description=
+"""This program will convert a BIDS MRI dataset to a NIDM-Experiment RDF document.  It will parse phenotype information and simply store variables/values and link to the associated json data dictionary file.\n\n
+Example 1: No variable->term mapping, simple BIDS dataset conversion which will add nidm.ttl file to BIDS dataset and .bidsignore file:
+\t BIDSMRI2NIDM.py -d [root directory of BIDS dataset] -bidsignore
+Example 2: No variable->term mapping, simple BIDS dataset conversion but storing nidm file somewhere else: \n
+\t BIDSMRI2NIDM.py -d [root directory of BIDS dataset] -o [PATH/nidm.ttl] \n\n
+Example 3: BIDS conversion with variable->term mappings, no existing mappings available, uses Interlex for terms and github, adds nidm.ttl file BIDS dataset and .bidsignore file: \n
+\t BIDSMRI2NIDM.py -d [root directory of BIDS dataset] -ilxkey [Your Interlex key] -github [username token] -bidsignore  \n\n
+Example 4: BIDS conversion with variable->term mappings, no existing mappings available, uses Interlex + NIDM OWL file for terms and github, adds nidm.ttl file BIDS dataset and .bidsignore file: \n
+\t BIDSMRI2NIDM.py -d [root directory of BIDS dataset] -ilxkey [Your Interlex key] -github [username token] -owl -bidsignore  \n\n
+Example 5 (FULL MONTY): BIDS conversion with variable->term mappings, uses JSON mapping file first then uses Interlex + NIDM OWL file for terms and github, adds nidm.ttl file BIDS dataset and .bidsignore file: \n
+\t BIDSMRI2NIDM.py -d [root directory of BIDS dataset] -json_map [Your JSON file] -ilxkey [Your Interlex key] -github [username token] -owl -bidsignore\n
+\t json mapping file has entries for each variable with mappings to formal terms.  Example:  \n
+    \t { \n
+    \t\t \"site\": { \n
+	\t\t \"definition\": \"Number assigned to site\", \n
+	\t\t \"label\": \"site_id (UC Provider Care)\", \n
+	\t\t \"url\": \"http://uri.interlex.org/NDA/uris/datadictionary/elements/2031448\" \n
+	\t\t }, \n
+	\t\t \"gender\": { \n
+	\t\t \"definition\": \"ndar:gender\", \n
+	\t\t \"label\": \"ndar:gender\", \n
+	\t\t \"url\": \"https://ndar.nih.gov/api/datadictionary/v2/dataelement/gender\" \n
+	\t\t } \n
+    \t }""" ,formatter_class=RawTextHelpFormatter)
 
     parser.add_argument('-d', dest='directory', required=True, help="Path to BIDS dataset directory")
     parser.add_argument('-jsonld', '--jsonld', action='store_true', help='If flag set, output is json-ld not TURTLE')
     parser.add_argument('-png', '--png', action='store_true', help='If flag set, tool will output PNG file of NIDM graph')
     parser.add_argument('-bidsignore', '--bidsignore', action='store_true', default = False, help='If flag set, tool will add NIDM-related files to .bidsignore file')
     #adding argument group for var->term mappings
-    mapvars_group = parser.add_argument_group('Map Variables to Terms')
+    mapvars_group = parser.add_argument_group('map variables to terms arguments')
     mapvars_group.add_argument('-json_map', '--json_map', dest='json_map',required=False,default=False,help="Optional user-suppled JSON file containing variable-term mappings.")
     mapvars_group.add_argument('-ilxkey', '--ilxkey', dest='key', required=False, default=None,  help="Interlex/SciCrunch API key to use for query")
-    mapvars_group.add_argument('-github','--github', type=str, nargs='*', default = None,dest='github',  required=False, help='Use -github flag with list username,token(or pw) for storing locally-defined terms in a \
-                    \"nidm-local-terms\" repository in GitHub.  If user doesn''t supply a token then user will be prompted for username/password.\n \
-                    Example: -github username token')
+    mapvars_group.add_argument('-github','--github', type=str, nargs='*', default = None,dest='github',  required=False, help="""Use -github flag with list username token(or pw) for storing locally-defined terms in a
+    nidm-local-terms repository in GitHub.  If user doesn''t supply a token then user will be prompted for username/password.\n
+    Example: -github username token""")
     mapvars_group.add_argument('-owl', action='store_true', required=False, default=None,help='Optional flag to query nidm-experiment OWL files')
     #parser.add_argument('-mapvars', '--mapvars', action='store_true', help='If flag set, variables in participant.tsv and phenotype files will be interactively mapped to terms')
-    parser.add_argument('-o', dest='outputfile', default="nidm.ttl", help="Outputs turtle file called nidm.ttl in BIDS directory by default")
+    parser.add_argument('-o', dest='outputfile', required=False, default="nidm.ttl", help="Outputs turtle file called nidm.ttl in BIDS directory by default")
 
     args = parser.parse_args()
 
