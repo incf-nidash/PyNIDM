a8b23ddd237317b6d20ac11738e9402387e733ed
Al <crowley@tcg.com>
2019-10-21 14:22:21 -0400

    Now return the instrumnet data along with subjects in REST API calls

---
+++ docker/rundatalad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#  You can create the docker container with:
+#  docker build -t pynidm .
+#
+# The command below will mount the ~/PyNIDM and ~/simple2_NIDM_examples directories
+# in /opt and give you a bash shell in the container
+
+sudo docker run -v ~/PyNIDM:/opt/project -v ~/simple2_NIDM_examples:/opt/simple2_NIDM_examples -it datalad
