61b6ae621e92a6da2a3294378e65880221a6607d
David Keator <dbkeator@LTM2737.hs.uci.edu>
2020-03-18 14:01:04 -0700

    adding nidm file format converter

--- nidm/experiment/tools/click_main.py
+++ nidm/experiment/tools/click_main.py
@@ -4,3 +4,4 @@ from nidm.experiment.tools import nidm_query
 from nidm.experiment.tools import nidm_visualize
 from nidm.experiment.tools import nidm_concat
 from nidm.experiment.tools import nidm_merge
+from nidm.experiment.tools import nidm_convert
