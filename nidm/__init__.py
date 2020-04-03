0b828ba4260a840ecd09e77e7328200833aa02d0
David Keator <dbkeator@uci.edu>
2020-04-02 17:01:45 -0700

    updating readme and adding etelemetry support

--- nidm/__init__.py
+++ nidm/__init__.py
@@ -1,2 +1,7 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__
+import __main__
+
+if not hasattr(__main__, "__file__"):
+    import etelemetry
+    etelemetry.check_available_version("pynidm", __version__)
