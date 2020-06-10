ddcd4f3038f776bd29131462dbcbf7d2bb894c84
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2020-06-09 22:56:06 -0400

    simplest change to allow etelemetry tracking

--- nidm/__init__.py
+++ nidm/__init__.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division, print_function
 from .version import __version__
 import __main__
 
-if not hasattr(__main__, "__file__"):
+try:
     import etelemetry
     etelemetry.check_available_version("incf-nidash/pynidm", __version__)
+except ImportError:
+    pass
