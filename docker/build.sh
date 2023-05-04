ebf53796c3369e5b6ba0448956aa56fa339e7f2a
John T. Wodder II <git@varonathe.org>
2023-05-04 09:40:39 -0400

    Clean up Docker-related files

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../cache
-sudo docker build -f Dockerfile -t pynidm .
-sudo docker build -f Dockerfile-rest -t pynidm-rest .
--- docker/rundocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#  You can create the docker container with the build.sh script in this directory
-#
-# The command below will mount the ~/PyNIDM and ~/simple2_NIDM_examples directories
-# in /opt and give you a bash shell in the container
-
-sudo docker run -v ~/PyNIDM:/opt/project -v ~/simple2_NIDM_examples:/opt/simple2_NIDM_examples  -it pynidm
