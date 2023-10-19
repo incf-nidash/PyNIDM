3298492d54506fb4747e0fd423a97c0a9e72cdf2
Yaroslav Halchenko <debian@onerussian.com>
2023-10-19 13:27:26 -0400

    Add my_isinstance function to alert whenever we have mismatch in doing the comparison

--- src/nidm/__init__.py
+++ src/nidm/__init__.py
@@ -8,3 +8,10 @@ try:
     etelemetry.check_available_version("incf-nidash/pynidm", __version__)
 except ImportError:
     pass
+
+
+def my_isinstance(o, t):
+    if (type(o) is t) != isinstance(o, t):
+        #import pdb; pdb.set_trace()
+        print(f"difference: o {o} is of type {type(o)}, t is {t}, isinstance(o, t): {isinstance(o, t)}")
+    return isinstance(o, t)
